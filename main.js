3bcfa8474ea72371da8ae755bd924a749364061f
Daniel Han <mrhanner@gmail.com>
2022-01-26 21:25:33 +0000

    logic change

--- main.js
+++ main.js
@@ -1,41 +1,63 @@
 
+const input = document.querySelector("#input");
+let numbers = document.querySelectorAll(".num");
+let operator = document.querySelectorAll(".operator");
+let equals = document.querySelector(".equal-sign");
+const clear = document.getElementById(".all-clear");
+
+let firstNumber = document.querySelector("firstNumber");
+let secondNumber = document.querySelector("secondNumber");
+let symbol = document.querySelectorAll(".operator");
+let result;
 
 
+numbers.forEach(item => {
+  item.addEventListener("click", event => {
+    input.innerHTML += item.innerHTML;
+    firstNumber = input.innerHTML;
+    console.log(firstNumber);
+  })  
+})
 
-calculator = {
-
-  input: document.querySelectorAll(".input"),  
-  numberButtons: document.querySelectorAll("num"),  
-  operatorButtons: document.querySelectorAll("operator"), 
-  equalsButton: document.querySelector("equal-sign"),
-  deleteButton: document.querySelector("all-clear"),
-  previousOperand: document.querySelector("previousoperand"),
-  currentOperand: document.querySelector("currentoperand"),
-  eventCalculator: document.querySelector(".calculator").addEventListener("click", (event) => {
-  const calcButton = event.target;
-  switch (calcButton.className) {
-    case "num":
-      input.innerHTML += calcButton.innerHTML;
-      // currCalculation.push(calcButton.innerHTML);
-      break;
-    case "all-clear":
-      input.innerHTML = "";
-      // currCalculation.push(calcButton.innerHTML);
-      break;
-    case "operator":
-      input.innerHTML = calcButton.innerHTML;
-      // currCalculation.push(calcButton.innerHTML);
-      break; 
-    case "equal-sign":
-      input.innerHTML = calcButton.innerHTML;
-      // currCalculation.push(calcButton.innerHTML);
-      
+operator.forEach(item => {
+  item.addEventListener("click", event => {
+    
+    input.innerHTML =" ";
+    symbol = input.innerHTML;
+   
+    console.log(operator);
     
 
-        
+    
+  
+   
+    
 
+  })
+})
+
+
+equals.addEventListener("click", event => {
+  secondNumber = input.innerHTML;
+  console.log(secondNumber);
+ 
+})
+
+
+
+function calculate( firstNumber, secondNumber, operator) {
+  if (operator === '+') {
+    return firstNumber + secondNumber;
+  } else if (operator === '-') {
+    return firstNumber - secondNumber;
+  } else if (operator === '*') {
+    return firstNumber * secondNumber;
+  } else if (operator === '/') {
+    return firstNumber / secondNumber;
   }
+  console.log(calculate);
+  
 }
-)
 
-}
+
+
