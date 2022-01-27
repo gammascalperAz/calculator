5d47ab43b42e5becdd0605366ad77bcf5c2f7796
Daniel Han <mrhanner@gmail.com>
2022-01-27 06:41:00 +0000

    relogic

--- main.js
+++ main.js
@@ -1,63 +1,97 @@
 
+
+
+
+
+
+// variables
+
 const input = document.querySelector("#input");
 let numbers = document.querySelectorAll(".num");
-let operator = document.querySelectorAll(".operator");
-let equals = document.querySelector(".equal-sign");
-const clear = document.getElementById(".all-clear");
+let operators = document.querySelectorAll(".operator");
+let equals = document.getElementById("equal-sign");
+const clear = document.getElementById("all-clear");
 
 let firstNumber = document.querySelector("firstNumber");
 let secondNumber = document.querySelector("secondNumber");
 let symbol = document.querySelectorAll(".operator");
 let result;
 
+// array for calculation
+let calcArr = [];
 
-numbers.forEach(item => {
-  item.addEventListener("click", event => {
-    input.innerHTML += item.innerHTML;
-    firstNumber = input.innerHTML;
-    console.log(firstNumber);
-  })  
-})
+// cycling through  numbers and displaying result
+numbers.forEach(number => {
+  number.addEventListener("click", event => {
+    if (input.innerText === "+") {
+      input.innerText = "";
+    } else if (input.innerText === "-") {
+      input.innerText = "";
+    } else if (input.innerText === "*") {
+      input.innerText = "";
+    } else if (input.innerText === "/") {
+      input.innerText = "";
 
-operator.forEach(item => {
-  item.addEventListener("click", event => {
+    }
+    input.innerText += number.innerHTML;
     
-    input.innerHTML =" ";
-    symbol = input.innerHTML;
-   
-    console.log(operator);
-    
+  });  
+});
+
+operators.forEach(operator => {
+  operator.addEventListener("click", event => {
+    calcArr.push(input.innerText);
+    input.innerText = operator.innerHTML;
+    calcArr.push(operator.innerHTML);
+    console.log(calcArr);
 
     
-  
-   
-    
-
-  })
-})
+  });
+});
 
 
 equals.addEventListener("click", event => {
-  secondNumber = input.innerHTML;
-  console.log(secondNumber);
- 
-})
-
-
-
-function calculate( firstNumber, secondNumber, operator) {
-  if (operator === '+') {
-    return firstNumber + secondNumber;
-  } else if (operator === '-') {
-    return firstNumber - secondNumber;
-  } else if (operator === '*') {
-    return firstNumber * secondNumber;
-  } else if (operator === '/') {
-    return firstNumber / secondNumber;
-  }
-  console.log(calculate);
+  console.log(input.innerText);
+  calcArr.push(input.innerText);
+  input.innerText = equals.innerHTML;
+  calcArr.push(equals.innerHTML);
+  let total = 0;
+  switch (calcArr[1]) {
+    case "+":
+      total = Number(calcArr[0]) + Number(calcArr[2]);
+      break;
+    case "-":
+      total = Number(calcArr[0]) - Number(calcArr[2]);
+      break;
+    case "/":
+      total = Number(calcArr[0]) / Number(calcArr[2]);
+      break;
+    case "*":
+      total = Number(calcArr[0]) * Number(calcArr[2]);
+      break;  
+      
   
-}
+  }
+  
+  input.innerText = total;    
+ 
+});
 
 
 
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+  
